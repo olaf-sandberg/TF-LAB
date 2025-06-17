8dbc8f9e73c4015f509c71f5a4d85bcb0601795c
olaf-sandberg <sandberg.olaf@gmail.com>
2025-06-17 12:31:28 +0200

    lab000 dynamic changes

---
+++ lab000-dynamic_lab/terraform.tfvars
@@ -0,0 +1,3 @@
+ami = "ami-xxxxx"
+ec2_type = "t3.nano"
+subnet_id = "subnet-yyyy"
---
+++ lab000-dynamic_lab/vars.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+    type = "string"
+    default = "ami-xxxx"  
+}
+
+variable "ec2_type" {
+    type = "string"
+    default = "t2.micro"
+}
+
+variable "subnet_id" {
+    type = "string"
+    default = "subnet-xxx"
+}
+
