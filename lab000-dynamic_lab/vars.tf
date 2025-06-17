dae88345011bfe8b75b4a7afe754d0f9eef7e44a
olaf-sandberg <sandberg.olaf@gmail.com>
2025-06-17 13:11:26 +0200

    lab000 dynamic changes

--- lab000-dynamic_lab/vars.tf
+++ lab000-dynamic_lab/vars.tf
@@ -1,15 +1,15 @@
 variable "ami" {
-    type = "string"
-    default = "ami-xxxx"  
+    type = string
+    default = "ami-04d20b438ef4a018a"  
 }
 
 variable "ec2_type" {
-    type = "string"
+    type = string
     default = "t2.micro"
 }
 
 variable "subnet_id" {
-    type = "string"
-    default = "subnet-xxx"
+    type = string
+    default = "subnet-04e8f57d7b4b7544f"
 }
 
