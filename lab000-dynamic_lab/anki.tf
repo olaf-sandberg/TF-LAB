f3e9ac4ad439af7f2e8e408d6c2298bcd9c64878
olaf-sandberg <sandberg.olaf@gmail.com>
2025-06-24 09:42:50 +0200

    lab000 dynamic changes

--- lab000-dynamic_lab/anki.tf
+++ lab000-dynamic_lab/anki.tf
@@ -19,11 +19,10 @@ terraform {
 
 provider "aws" {
   region = "eu-central-1"
-  alias = "eu"
-  
 }
 
 resource "aws_instance" "ec2-olo" {
+  provider = aws
   subnet_id = "subnet-0c66c7cba1beba439"
   ami = "ami-04d20b438ef4a018a"
   instance_type = "t2.nano"
