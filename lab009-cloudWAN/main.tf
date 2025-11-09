50652022678bb9daf5992b9ef31bc3ada108507a
olaf-sandberg <sandberg.olaf@gmail.com>
2025-11-09 15:53:34 +0100

    lab009 workloads

--- lab009-cloudWAN/main.tf
+++ lab009-cloudWAN/main.tf
@@ -89,3 +89,38 @@ module "vpc_e" {
     VPC         = "E"
   }
 }
+
+
+
+
+
+module "sg_central-1" {
+  source     = "./modules/security/SG"
+  providers  = { aws = aws.eu_central_1 }
+
+  sg_name     = "sg-eu-1"
+  vpc_id      = module.vpc_a.vpc_id
+  environment = "dev"
+  project     = "cloudwan-lab"
+}
+
+
+module "sg_west-1" {
+  source     = "./modules/security/SG"
+  providers  = { aws = aws.eu_west_1 }
+
+  sg_name     = "sg-west-1"
+  vpc_id      = module.vpc_a.vpc_id
+  environment = "dev"
+  project     = "cloudwan-lab"
+}
+
+module "sg_west-2" {
+  source     = "./modules/security/SG"
+  providers  = { aws = aws.eu_west_2 }
+
+  sg_name     = "sg-west-2"
+  vpc_id      = module.vpc_a.vpc_id
+  environment = "dev"
+  project     = "cloudwan-lab"
+}
