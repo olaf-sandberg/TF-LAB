85119a98ccf9221d55f32387ef56e51a9dc4752b
olaf-sandberg <sandberg.olaf@gmail.com>
2025-11-09 15:19:43 +0100

    lab009 workloads

--- lab009-cloudWAN/main.tf
+++ lab009-cloudWAN/main.tf
@@ -33,3 +33,59 @@ module "vpc_b" {
     VPC         = "B"
   }
 }
+
+module "vpc_c" {
+  source = "./modules/network/vpc"
+
+  providers = {
+    aws = aws.eu_west_1
+  }
+
+  region         = "eu-west-1"
+  project        = "cloudwan-lab"
+  cidr           = "10.100.0.0/16"
+  public_subnets = ["10.1000.0.0/24"]
+
+  tags = {
+    Environment = "shared"
+    VPC         = "C"
+  }
+}
+
+
+module "vpc_ad" {
+  source = "./modules/network/vpc"
+
+  providers = {
+    aws = aws.eu_west_2
+  }
+
+  region         = "eu-west-2"
+  project        = "cloudwan-lab"
+  cidr           = "192.168.0.0/24"
+  public_subnets = ["192.168.0.0/25"]
+
+  tags = {
+    Environment = "dev"
+    VPC         = "D"
+  }
+}
+
+
+module "vpc_ad" {
+  source = "./modules/network/vpc"
+
+  providers = {
+    aws = aws.eu_west_2
+  }
+
+  region         = "eu-west-2"
+  project        = "cloudwan-lab"
+  cidr           = "192.168.1.0/24"
+  public_subnets = ["192.168.1.0/25"]
+
+  tags = {
+    Environment = "prod"
+    VPC         = "E"
+  }
+}
