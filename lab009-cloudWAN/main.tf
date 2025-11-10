4aa649225b736f838ca3cb54312a1f479e4f745a
olaf-sandberg <sandberg.olaf@gmail.com>
2025-11-10 09:34:29 +0100

    lab009 workloads

--- lab009-cloudWAN/main.tf
+++ lab009-cloudWAN/main.tf
@@ -144,3 +144,79 @@ module "sg_west_2e" {
   environment = "dev"
   project     = "cloudwan-lab"
 }
+
+
+# ---------- EC2 A (eu-central-1 / VPC A / DEV) ----------
+module "ec2_a" {
+  source    = "./modules/compute/ec2"
+  providers = { aws = aws.eu_central_1 }
+
+  instance_name = "ec2-a"
+  instance_type = "t3.micro"
+  ami_id        = "ami-04d20b438ef4a018a"
+  subnet_id     = module.vpc_a.public_subnet_ids[0]
+  sg_id         = module.sg_central_1a.sg_id
+  key_name      = "cloudwan-key"
+  environment   = "dev"
+  project       = "cloudwan-lab"
+}
+
+# ---------- EC2 B (eu-central-1 / VPC B / PROD) ----------
+module "ec2_b" {
+  source    = "./modules/compute/ec2"
+  providers = { aws = aws.eu_central_1 }
+
+  instance_name = "ec2-b"
+  instance_type = "t3.micro"
+  ami_id        = "ami-04d20b438ef4a018a"
+  subnet_id     = module.vpc_b.public_subnet_ids[0]
+  sg_id         = module.sg_central_1b.sg_id
+  key_name      = "cloudwan-key"
+  environment   = "prod"
+  project       = "cloudwan-lab"
+}
+
+# ---------- EC2 C (eu-west-1 / VPC C / SHARED) ----------
+module "ec2_c" {
+  source    = "./modules/compute/ec2"
+  providers = { aws = aws.eu_west_1 }
+
+  instance_name = "ec2-c"
+  instance_type = "t3.micro"
+  ami_id        = "ami-04d20b438ef4a018a"
+  subnet_id     = module.vpc_c.public_subnet_ids[0]
+  sg_id         = module.sg_west_1c.sg_id
+  key_name      = "cloudwan-key"
+  environment   = "shared"
+  project       = "cloudwan-lab"
+}
+
+# ---------- EC2 D (eu-west-2 / VPC D / DEV) ----------
+module "ec2_d" {
+  source    = "./modules/compute/ec2"
+  providers = { aws = aws.eu_west_2 }
+
+  instance_name = "ec2-d"
+  instance_type = "t3.micro"
+  ami_id        = "ami-04d20b438ef4a018a"
+  subnet_id     = module.vpc_d.public_subnet_ids[0]
+  sg_id         = module.sg_west_2d.sg_id
+  key_name      = "cloudwan-key"
+  environment   = "dev"
+  project       = "cloudwan-lab"
+}
+
+# ---------- EC2 E (eu-west-2 / VPC E / PROD) ----------
+module "ec2_e" {
+  source    = "./modules/compute/ec2"
+  providers = { aws = aws.eu_west_2 }
+
+  instance_name = "ec2-e"
+  instance_type = "t3.micro"
+  ami_id        = "ami-04d20b438ef4a018a"
+  subnet_id     = module.vpc_e.public_subnet_ids[0]
+  sg_id         = module.sg_west_2e.sg_id
+  key_name      = "cloudwan-key"
+  environment   = "prod"
+  project       = "cloudwan-lab"
+}
