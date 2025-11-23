55d25c415578d88a81ff6f05203b87328e026af9
olaf-sandberg <sandberg.olaf@gmail.com>
2025-11-23 12:29:14 +0100

    lab010 upload

---
+++ lab010-terragrunt/live/terragrunt.hcl
@@ -0,0 +1,35 @@
+locals {
+  # global tags applied to all resources
+  default_tags = {
+    project     = "terragrunt-lab"
+    environment = "lab"
+    owner       = "olo"
+  }
+}
+
+# all child folders inherit this
+generate "provider" {
+  path      = "provider.tf"
+  if_exists = "overwrite"
+  contents  = <<EOF
+provider "aws" {
+  region = var.region
+}
+EOF
+}
+
+remote_state {
+  backend = "s3"
+
+  config = {
+    bucket         = "smartbidit"
+    key            = "./terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
+inputs = {
+  global_tags = local.default_tags
+}
---
+++ lab010-terragrunt/modules/TEMP_VPC_EC2_LAB_MODULE/main.tf
@@ -0,0 +1,29 @@
+variable "vpc_cidr" {}
+variable "region" {}
+variable "name" {}
+variable "global_tags" {}
+
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+  tags       = merge(var.global_tags, { Name = "${var.name}-vpc" })
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 4, 1)
+  availability_zone = "${var.region}a"
+
+  tags = merge(var.global_tags, { Name = "${var.name}-subnet" })
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-08d4ac5b634553e16" # Ubuntu 22.04 (eu-central-1)
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.subnet.id
+
+  tags = merge(var.global_tags, { Name = "${var.name}-ec2" })
+}
+
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
